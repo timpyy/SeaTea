0b74ff0c045a42c33891a3324a202d35632e8240
Tim Pornyuenyong <teakpyy@gmail.com>
2023-05-03 14:48:21 -0700

    Modified the product fields in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -3,8 +3,10 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    //Create a Product Model
     public class ProductModel
     {
+        //Defining the various attributes that a Product Model holds
         public string Id { get; set; }
         public string Maker { get; set; }
         
@@ -13,6 +15,8 @@ namespace ContosoCrafts.WebSite.Models
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string OnlineMenuLink { get; set; }
+        public string Phone { get; set; }
         public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
