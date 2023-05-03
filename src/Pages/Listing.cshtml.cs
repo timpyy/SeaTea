59e7e97f650c179ba195ed68c51b5775242867af
Tim Pornyuenyong <teakpyy@gmail.com>
2023-05-03 13:32:40 -0700

    Renamed listing3.cshtml.cs to Listing.cshtmlc.cs

---
+++ src/Pages/Listing.cshtml.cs
@@ -0,0 +1,29 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class ListingModel : PageModel
+    {
+        public ListingModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
