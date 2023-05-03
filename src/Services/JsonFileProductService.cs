f335e85e1b52c8fc6c0ac1c234cd3af97c69b2e5
Tim Pornyuenyong <teakpyy@gmail.com>
2023-05-03 14:47:40 -0700

    Added methods for Create and Update in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -9,19 +9,23 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
+        //Create JsonFileProductService on the webHostEnvironment
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
+        //Define a getter for WebHostEnvironment
         public IWebHostEnvironment WebHostEnvironment { get; }
 
+        //Establish the file path to products.json, where all the products are stored
         private string JsonFileName
         {
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
+        //Method that goes through products.json and deserializes/returns all product data that is contained within
         public IEnumerable<ProductModel> GetAllData()
         {
             using (var jsonFileReader = File.OpenText(JsonFileName))
@@ -34,9 +38,10 @@ namespace ContosoCrafts.WebSite.Services
             }
         }
 
+        //Method that returns all ProductModel objects from products.json
         public IEnumerable<ProductModel> GetProducts()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -46,11 +51,20 @@ namespace ContosoCrafts.WebSite.Services
             }
         }
 
+        /// <summary>
+        /// Add Rating
+        /// 
+        /// Take in the product ID and the rating
+        /// If the rating does not exist, add it
+        /// Save the update
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="rating"></param>
         public void AddRating(string productId, int rating)
         {
             var products = GetProducts();
 
-            if(products.First(x => x.Id == productId).Ratings == null)
+            if (products.First(x => x.Id == productId).Ratings == null)
             {
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
             }
@@ -61,17 +75,89 @@ namespace ContosoCrafts.WebSite.Services
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
             }
 
-            using(var outputStream = File.OpenWrite(JsonFileName))
+            using (var outputStream = File.OpenWrite(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
                     {
                         SkipValidation = true,
                         Indented = true
-                    }), 
+                    }),
                     products
                 );
             }
+        }
+
+        /// <summary>
+        /// Private method that Saves All products data to storage
+        /// </summary>
+        private void SaveData(IEnumerable<ProductModel> products)
+        {
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+
+        /// <summary>
+        /// Create a new product using default values
+        /// After create the user can update to set values
+        /// </summary>
+        /// <returns></returns>
+        public ProductModel CreateData()
+        {
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Title = "Enter Store Name",
+                Description = "Enter Store Address",
+                Url = "Enter Store URL",
+                Image = "Enter Store Image URL",
+                Phone = "Enter Store's Phone Number",
+                OnlineMenuLink = "Enter URL to Store's menu",
+            };
+
+            //Get the current set, and append the new record to it
+            var dataSet = GetAllData();
+            dataSet = dataSet.Append(data);
+
+            SaveData(dataSet);
+
+            return data;
+        }
+
+        /// <summary>
+        /// Find the data record
+        /// Update the fields
+        /// Save to the data store
+        /// </summary>
+        /// <param name="data"></param>
+        public ProductModel UpdateData(ProductModel data)
+        {
+            var products = GetAllData();
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+            if (productData == null)
+            {
+                return null;
+            }
+            // Update the data to the new passed in values
+            productData.Title = data.Title;
+            productData.Description = data.Description.Trim();
+            productData.Url = data.Url;
+            productData.Image = data.Image;
+            productData.Phone = data.Phone;
+            productData.OnlineMenuLink = data.OnlineMenuLink;
+
+            SaveData(products);
+
+            return productData;
         }
     }
 }
