3ebdde5695155bda6c99f7d18248846de70ae3cd
yashatre98 <yashatre98@gmail.com>
2023-05-03 22:22:45 -0700

    added unit tests to Read.cshtml.Tests.cs

---
+++ UnitTests/Pages/Read.cshtml.Tests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+
+
+using ContosoCrafts.WebSite.Pages;
+
+
+
+namespace UnitTests.Pages.Read
+
+{
+
+    public class ReadTests
+
+    {
+
+        #region TestSetup
+
+        public static ReadModel pageModel;
+
+
+
+        [SetUp]
+
+        public void TestInitialize()
+
+        {
+
+            pageModel = new ReadModel(TestHelper.ProductService)
+
+            {
+
+            };
+
+        }
+
+
+
+        #endregion TestSetup
+
+
+
+        #region OnGet
+
+        [Test]
+
+        public void OnGet_Valid_Should_Return_Products()
+
+        {
+
+            // Arrange
+
+
+
+            // Act
+
+            pageModel.OnGet("Boba Up");
+
+
+
+            // Assert
+
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+
+            Assert.AreEqual("Boba Up - Self Serve", pageModel.Product.Title);
+
+        }
+
+        #endregion OnGet
+
+    }
+
+}
+
+
+
+
+
