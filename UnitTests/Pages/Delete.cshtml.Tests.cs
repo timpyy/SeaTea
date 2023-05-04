0e6ce0e79f4039a044d564f388f81083162c7903
yashatre98 <yashatre98@gmail.com>
2023-05-03 22:21:38 -0700

    Added Unit tests to Delete.cshtml.Tests.cs

---
+++ UnitTests/Pages/Delete.cshtml.Tests.cs
@@ -0,0 +1,117 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Delete
+{
+    public class DeleteTests
+    {
+        #region TestSetup
+        public static DeleteModel pageModel;
+
+
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new DeleteModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+
+
+        #endregion TestSetup
+
+
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+
+
+            // Act
+            pageModel.OnGet("Boba Up");
+
+
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Boba Up - Self Serve", pageModel.Product.Title);
+        }
+        #endregion OnGet
+
+
+
+        #region OnPostAsync
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+
+
+            // First Create the product to delete
+            pageModel.Product = TestHelper.ProductService.CreateData();
+            pageModel.Product.Title = "Example to Delete";
+            TestHelper.ProductService.UpdateData(pageModel.Product);
+
+
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Listing"));
+
+
+
+            // Confirm the item is deleted
+            Assert.AreEqual(null, TestHelper.ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(pageModel.Product.Id)));
+        }
+
+
+
+        [Test]
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "bogus",
+                Title = "bogus",
+                Description = "bogus",
+                Url = "bogus",
+                Image = "bougs"
+            };
+
+
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+
+
+            // Act
+            var result = pageModel.OnPost() as ActionResult;
+
+
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPostAsync
+    }
+}
