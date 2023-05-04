cbde64311d6c4ceeb76296fcd13625acf37d5c1b
yashatre98 <yashatre98@gmail.com>
2023-05-03 22:50:32 -0700

    Added unit tests to Listing.cshtml.Tests.cs

---
+++ UnitTests/Pages/Listing.cshtml.Tests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages;
+
+
+
+namespace UnitTests.Pages.Listing
+{
+    public class ListingTests
+    {
+        #region TestSetup
+        public static PageContext pageContext;
+
+
+
+        public static ListingModel pageModel;
+
+
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ListingModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+
+
+        #endregion TestSetup
+
+
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+
+
+            // Act
+            pageModel.OnGet();
+
+
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true,pageModel.Products.ToList().Any());
+        }
+        #endregion OnGet
+    }
+}
