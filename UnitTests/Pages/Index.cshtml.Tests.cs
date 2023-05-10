6586a3dd8c80dac5110c518f77eb87918f698018
Tim Pornyuenyong <teakpyy@gmail.com>
2023-05-10 16:27:05 -0700

    Added UnitTest for Index page in Index.cshtml.Tests.cs

---
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Index
+{
+    public class IndexTests
+    {
+        #region TestSetup
+        public static IndexModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+
+            pageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
