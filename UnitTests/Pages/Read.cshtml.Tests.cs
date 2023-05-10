3de74042aef234dbf9b55050b232d19acb5513b1
thegiankaiser <thegiankaiser@gmail.com>
2023-05-09 23:54:13 -0700

    Added comments to Read.cshtml.Tests.cs

--- UnitTests/Pages/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Read.cshtml.Tests.cs
@@ -9,19 +9,23 @@ using ContosoCrafts.WebSite.Pages;
 namespace UnitTests.Pages.Read
 
 {
-
+    //<summary>
+    //The purpose of this class is to run a read test
+    //on the read method because we want to have a 100%
+    //coverage for our unit tests.
+    //</summary>
     public class ReadTests
 
     {
 
         #region TestSetup
-
+        //Sets up the testing environment
         public static ReadModel pageModel;
 
 
 
         [SetUp]
-
+        //Initilizes the unit test using the TestHelper class.
         public void TestInitialize()
 
         {
@@ -43,20 +47,16 @@ namespace UnitTests.Pages.Read
         #region OnGet
 
         [Test]
-
+        //Applies Unit Tests to the read method for the website. 
         public void OnGet_Valid_Should_Return_Products()
 
         {
 
             // Arrange
 
-
-
             // Act
 
             pageModel.OnGet("Boba Up");
-
-
 
             // Assert
 
