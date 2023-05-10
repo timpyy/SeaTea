9146a86bf16d85629b4f77d23f9ca4473bb23004
thegiankaiser <thegiankaiser@gmail.com>
2023-05-10 00:01:35 -0700

    Added comments to Listing.cshtml.Tests.cs

--- UnitTests/Pages/Listing.cshtml.Tests.cs
+++ UnitTests/Pages/Listing.cshtml.Tests.cs
@@ -7,18 +7,23 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Listing
 {
+    //<summary>
+    //The purpose of this class is to run a unit test
+    //on the index method because we want to have a 100%
+    //coverage for our unit tests.
+    //</summary>
     public class ListingTests
     {
         #region TestSetup
         public static PageContext pageContext;
 
-
-
+        //Inititates pagemodel to a ListingModel object
         public static ListingModel pageModel;
 
 
 
         [SetUp]
+        //Initilizes the unit test using the TestHelper class.
         public void TestInitialize()
         {
             pageModel = new ListingModel(TestHelper.ProductService)
@@ -34,16 +39,13 @@ namespace UnitTests.Pages.Listing
 
         #region OnGet
         [Test]
+        //Applies Unit Tests to the listing(index) method for the website. 
         public void OnGet_Valid_Should_Return_Products()
         {
             // Arrange
 
-
-
             // Act
             pageModel.OnGet();
-
-
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
