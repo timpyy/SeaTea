2594d063bdaa2ae5eb2f2da9725cbf4adf29be4f
thegiankaiser <thegiankaiser@gmail.com>
2023-05-10 19:23:47 -0700

    Added Privacy.cshtml.cs to Privacy.cshtml

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
@@ -6,10 +7,16 @@ namespace ContosoCrafts.WebSite.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private JsonFileProductService productService;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
+        }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, JsonFileProductService productService) : this(logger)
+        {
+            this.productService = productService;
         }
 
         public void OnGet()
