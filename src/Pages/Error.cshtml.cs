c0d2ab2201a3d2d5a0809663f8d908c284815727
thegiankaiser <thegiankaiser@gmail.com>
2023-05-10 18:32:55 -0700

    Added comments to Error.cshtml.cs file.

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -3,15 +3,20 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-// add comment by Yash Atre, will be synced.
+// The purpose of this file is to provide
+// or display error if a error does occur
+// when running the file.
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    //Creates an error model from the page model class
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        //Initiates requestId as a string.
         public string RequestId { get; set; }
 
+        //Inititates a boolen for ShowRequestID.
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
@@ -20,7 +25,7 @@ namespace ContosoCrafts.WebSite.Pages
         {
             _logger = logger;
         }
-
+        //Provides the error message.
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
