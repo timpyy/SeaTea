7d5179c461f5634d0281e962d99cd96cf5a8970a
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 14:55:24 -0700

    Update TestBUnitHelper.cs.Tests.cs file

--- UnitTests/TestBUnitHelper.cs.Tests.cs
+++ UnitTests/TestBUnitHelper.cs.Tests.cs
@@ -5,44 +5,44 @@ namespace UnitTests
 {
 
     [TestFixture]
-    //<<<Summary>>>
-    // The purpose of this class is to create a testContext class that is a child of the 
-    // BunitTestContext to that we can have the appropriate credentials to test the SetUp and TearDown
-    // functions within the BunitTestContext
-    // <<</Summary>>>
+    ///<<<Summary>>>
+    /// The purpose of this class is to create a testContext class that is a child of the 
+    /// BunitTestContext to that we can have the appropriate credentials to test the SetUp and TearDown
+    /// functions within the BunitTestContext
+    /// <<</Summary>>>
 
     public class testContext : BunitTestContext
     {
     }
-    //<<<Summary>>>
-    // The purpose of this class is to run the testContext class SetUp and TearDown functions
-    // to ensure that the when UnitTest creates a TestBUnitHelper the classes functions are valid
-    // <<</Summary>>>
+    ///<<<Summary>>>
+    /// The purpose of this class is to run the testContext class SetUp and TearDown functions
+    /// to ensure that the when UnitTest creates a TestBUnitHelper the classes functions are valid
+    /// <<</Summary>>>
     public class BunitTestSetupTests
     {
         private testContext _testContext;
         [SetUp]
         public void SetUp()
         {
-            // Create an instance of the testContext class
+            /// Create an instance of the testContext class
             _testContext = new testContext();
         }
         [TearDown]
         public void TearDown()
         {
-            // Call the TearDown function to ensure it is valid
+            /// Call the TearDown function to ensure it is valid
             _testContext.TearDown();
         }
         [Test]
-        // Ensure that SetUp creates a valid BunitTestContext object
+        /// Ensure that SetUp creates a valid BunitTestContext object
         public void SetUpIsValid()
         {
-            // Arrange
+            /// Arrange
 
-            // Act
+            /// Act
             _testContext.Setup();
 
-            // Assert
+            /// Assert
             Assert.NotNull(_testContext);
         }
     }
