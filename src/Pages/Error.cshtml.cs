ec46b8b24e797c177187c4965306c7242f988640
thegiankaiser <thegiankaiser@gmail.com>
2023-05-16 19:32:46 -0700

    Update comments to Error.cshtml.cs file

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -3,32 +3,44 @@ using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-// The purpose of this file is to provide
-// or display error if a error does occur
-// when running the file.
+
+/// <summary>
+/// The purpose of this file is to provide
+/// or display error if a error does occur
+/// when running the file.
+/// </summary>
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    //Creates an error model from the page model class
+    /// <summary>
+    /// Creates an error model from the page model class
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        //Initiates requestId as a string.
+        ///Initiates requestId as a string and sets the id.
         public string RequestId { get; set; }
 
-        //Inititates a boolen for ShowRequestID.
+        /// <summary>
+        /// Inititates a boolen request for ShowRequestID.
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
         //private JsonFileProductService productService;
 
+        /// <summary>
+        /// Provides the error model if error does occur.
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
+            ///Logger is set to _logger class error model.
             _logger = logger;
         }
 
        
-        //Provides the error message.
+        ///Provides the error message.
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
