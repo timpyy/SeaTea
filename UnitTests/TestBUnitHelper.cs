8180fafb11d3c5105d801824f9019b9b4d6288b9
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 14:54:33 -0700

    Update comments in TestBUnitHelper.cs file

--- UnitTests/TestBUnitHelper.cs
+++ UnitTests/TestBUnitHelper.cs
@@ -3,18 +3,18 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
-    // <summary>
-    // The purpose of this class is to test the BUnitHelper
-    // because we want to run a 100% test coverage for our
-    // unit test.
-    // </summary>
+    /// <summary>
+    /// The purpose of this class is to test the BUnitHelper
+    /// because we want to run a 100% test coverage for our
+    /// unit test.
+    /// </summary>
     public abstract class BunitTestContext : TestContextWrapper
     {
-        // The Setup sets the context
+        /// The Setup sets the context
         [SetUp]
         public void Setup() => TestContext = new Bunit.TestContext();
 
-        // When done displose removes it, to free up system resources
+        /// When done displose removes it, to free up system resources
         [TearDown]
         public void TearDown() => TestContext.Dispose();
     }
