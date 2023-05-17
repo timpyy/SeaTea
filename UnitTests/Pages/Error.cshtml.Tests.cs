0487977732282eef04d07d34ffc3b1eb7d61cbe3
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 15:14:12 -0700

    Update comments in Error.cshtml.Tests.cs file

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -9,25 +9,31 @@ using System.Diagnostics;
 
 namespace UnitTests.Pages
 {
-    //<summary>
-    //The purpose of this class is to run a Error test
-    //on the Error method because we want to have a 100%
-    //coverage for our unit tests.
-    //</summary>
+    ///<summary>
+    ///The purpose of this class is to run a Error test
+    ///on the Error method because we want to have a 100%
+    ///coverage for our unit tests.
+    ///</summary>
     public class ErrorTests
     {
         #region TestSetup
         public static ErrorModel pageModel;
 
-        //Initializes the test for setup using the MockLoggerDirect.
+        /// <summary>
+        /// Initializes the test for setup using the MockLoggerDirect.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            //MockLoggerDirect is set to the error model of ILogger mock.
             var MockLoggerDirect = Mock.Of<ILogger<ErrorModel>>();
 
+            ///Page model is instantiated to a new error model.
             pageModel = new ErrorModel(MockLoggerDirect)
             {
+                ///PageContext is set to test helper page context.
                 PageContext = TestHelper.PageContext,
+                ///Tempory helper is set.
                 TempData = TestHelper.TempData,
             };
         }
@@ -36,20 +42,21 @@ namespace UnitTests.Pages
 
         #region OnGet
         [Test]
-        //Runs the unit test.
+        ///Runs the unit test.
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
-            // Arrange
+            /// Arrange
             
             Activity activity = new Activity("activity");
             activity.Start();
 
-            // Act
+            /// Act
             pageModel.OnGet();
 
-            // Reset
+            /// Reset
 
-            // Assert
+            /// Assert
+            /// Validates to see if the input data are valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual(activity.Id, pageModel.RequestId);
             Assert.AreEqual(true, pageModel.ShowRequestId);
@@ -59,18 +66,16 @@ namespace UnitTests.Pages
         #endregion OnGet
         #region OnGet
         [Test]
-        //Runs the unit test.
+        ///Runs the unit test.
         public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
         {
-            // Arrange
+            /// Arrange
 
-
-            // Act
+            /// Act
             pageModel.OnGet();
-
-            // Reset
-
-            // Assert
+            /// Reset
+            /// Assert
+            /// Validates to see if the input data are valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual("trace", pageModel.RequestId);
             Assert.AreEqual(true, pageModel.ShowRequestId);
