38f018c5c07c65a9e9a5a49b6bb82deab8e6c5a7
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 08:06:45 -0700

    Update comments to Index.cshtml.Tests.cs file

--- UnitTests/Pages/Product/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -7,47 +7,43 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Index
 {
-    //<summary>
-    //The purpose of this class is to run a unit test
-    //on the index method because we want to have a 100%
-    //coverage for our unit tests.
-    //</summary>
+    ///<summary>
+    ///The purpose of this class is to run a unit test
+    ///on the index method because we want to have a 100%
+    ///coverage for our unit tests.
+    ///</summary>
     public class ListingTests
     {
         #region TestSetup
         public static PageContext pageContext;
 
-        //Inititates pagemodel to a ListingModel object
+        ///Inititates pagemodel to a ListingModel object
         public static IndexModel pageModel;
 
-
-
         [SetUp]
-        //Initilizes the unit test using the TestHelper class.
+        ///Initilizes the unit test using the TestHelper class.
         public void TestInitialize()
         {
+            ///Sets pagemodel as an instantitated object of IndexModel
             pageModel = new IndexModel(TestHelper.ProductService)
             {
             };
         }
 
-
-
         #endregion TestSetup
-
-
 
         #region OnGet
         [Test]
-        //Applies Unit Tests to the listing(index) method for the website. 
+        ///Applies Unit Tests to the listing(index) method for the website. 
         public void OnGet_Valid_Should_Return_Products()
         {
-            // Arrange
+            /// Arrange
 
-            // Act
+            /// Act
             pageModel.OnGet();
 
             // Assert
+            /// Validates to see if input data is valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual(true,pageModel.Products.ToList().Any());
         }
