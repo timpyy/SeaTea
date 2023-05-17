3e265394abbd506a97e4878dc658042bbe1fd9d7
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 15:15:45 -0700

    Update comments in Index.cshtml.Tests.cs file

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -9,22 +9,27 @@ using System.Linq;
 
 namespace UnitTests.Pages.Index
 {
-    //<summary>
-    //The purpose of this class is to run a index test
-    //on the index method because we want to have a 100%
-    //coverage for our unit tests.
-    //</summary>
+    ///<summary>
+    ///The purpose of this class is to run a index test
+    ///on the index method because we want to have a 100%
+    ///coverage for our unit tests.
+    ///</summary>
     public class IndexTests
     {
         #region TestSetup
         public static IndexModel pageModel;
 
-        //Initializes the test for setup using the MockLoggerDirect.
+        /// <summary>
+        /// Initializes the test for setup using the MockLoggerDirect.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+
+            //MockLoggerDirect is set to the error model of ILogger mock.
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
 
+            ///Page model is instantiated to a new error model.
             pageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
             {
             };
@@ -34,17 +39,18 @@ namespace UnitTests.Pages.Index
 
         #region OnGet
         [Test]
-        //Runs the unit test.
+        ///Runs the unit test.
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
-            // Arrange
+            /// Arrange
 
-            // Act
+            /// Act
             pageModel.OnGet();
 
-            // Reset
+            /// Reset
 
             // Assert
+            /// Validates to see if the input data are valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
 
 
@@ -58,16 +64,13 @@ namespace UnitTests.Pages.Index
         [Test]
         public void OnGet_Should_Return_All_Products()
         {
-            // Arrange
+            /// Arrange
 
-
-
-            // Act
+            /// Act
             pageModel.OnGet();
 
-
-
-            // Assert
+            /// Assert
+            /// Validates to see if the input data are valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual(23, pageModel.Products.Count());
             
