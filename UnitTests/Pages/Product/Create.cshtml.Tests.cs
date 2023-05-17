713a039bf02c39030c35fd895a26507d50f022ef
thegiankaiser <thegiankaiser@gmail.com>
2023-05-17 08:02:29 -0700

    Update comments to Create.cshtml.Tests.cs file

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -7,42 +7,48 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Create
 {
-    //<summary>
-    //The purpose of this class is to run a unit test
-    //on the Create method because we want to have a 100%
-    //coverage for our unit tests.
-    //</summary>
+    ///<summary>
+    ///The purpose of this class is to run a unit test
+    ///on the Create method because we want to have a 100%
+    ///coverage for our unit tests.
+    ///</summary>
     public class CreateTests
     {
-        //Sets up the testing environment
+        ///Sets up the testing environment
         #region TestSetup
 
-        //Inititates pagemodel to a CreateModel object
+        ///Inititates pagemodel to a CreateModel object
         public static CreateModel pageModel;
 
         [SetUp]
-        //Initilizes the unit test using the TestHelper class.
+        ///Initilizes the unit test using the TestHelper class.
         public void TestInitialize()
         {
+            ///Creates a model for test helper that uses the
+            ///product service class on it
             pageModel = new CreateModel(TestHelper.ProductService)
             {
             };
         }
 
+        /// <summary>
+        /// Sets up the test environment for  unit testing.
+        /// </summary>
         #endregion TestSetup
 
         #region OnGet
         [Test]
-        //Applies Unit Tests to the create method for the website. 
+        ///Applies Unit Tests to the create method for the website. 
         public void OnGet_Valid_Should_Return_Products()
         {
-            // Arrange
+            /// Arrange
             var oldCount = TestHelper.ProductService.GetAllData().Count();
 
-            // Act
+            /// Act
             pageModel.OnGet();
 
-            // Assert
+            /// Assert
+            /// Validates to see if the input data are valid.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual(oldCount + 1, TestHelper.ProductService.GetAllData().Count());
         }
