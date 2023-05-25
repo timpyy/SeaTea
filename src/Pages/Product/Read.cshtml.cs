54e755282415f216b894876c80f5a5fe12d78cff
Tim Pornyuenyong <teakpyy@gmail.com>
2023-05-24 17:19:20 -0700

    Added error handling to OnGet in Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
@@ -33,9 +34,16 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// REST Get request
         /// </summary>
         /// <param name="id"></param>
-        public void OnGet(string id)
+        public IActionResult OnGet(string id)
         {
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+            //If invalid URL for read page provided, redirect to Index
+            if (Product == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
         }
     }
 }
